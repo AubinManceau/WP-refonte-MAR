97ab4a8060621fe95eddaef78cb271229a3a830a
Aubin MANCEAU <aubin.manceau@wemea.fr>
2026-02-18 18:05:50 +0100

    update

--- web/app/themes/marmite/template-gallery.php
+++ web/app/themes/marmite/template-gallery.php
@@ -14,7 +14,7 @@ get_header(); ?>
             if (!empty($gallery_ids) && is_array($gallery_ids)) : ?>
                 <div class="custom-gallery">
                     <?php foreach ($gallery_ids as $image_id) : 
-                        $image_url = wp_get_attachment_image_url($image_id);
+                        $image_url = wp_get_attachment_image_url($image_id, 'full');
                         $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', true);
                         ?>  
                         <img src="<?php echo esc_url($image_url); ?>" alt="<?php echo esc_attr($image_alt); ?>">
